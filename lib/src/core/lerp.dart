1b5f4e79a358538137c187cf400bba4734ff4158
Stanislav <qixi@mail.ru>
2021-06-11 20:22:38 +0300

    added lerpRadians

--- lib/src/core/lerp.dart
+++ lib/src/core/lerp.dart
@@ -1,3 +1,5 @@
+import 'math.dart';
+
 class Lerp {
   static double lerp(double start, double target, double duration, double timeSinceStart) {
     if (timeSinceStart > 0.0 && timeSinceStart < duration) {
@@ -24,5 +26,47 @@ class Lerp {
       return target;
     }
     return start;
+  }
+
+  static double lerpRadians(double start, double target, double duration, double timeSinceStart) {
+    double result;
+    final range = target - start;
+    if (range < -pi) {
+      target += twoPi;
+      result = lerp(start, target, duration, timeSinceStart);
+      if (result >= twoPi) {
+        result -= twoPi;
+      }
+    } else if (range > pi) {
+      target -= twoPi;
+      result = lerp(start, target, duration, timeSinceStart);
+      if (result < 0) {
+        result += twoPi;
+      }
+    } else {
+      result = lerp(start, target, duration, timeSinceStart);
+    }
+    return result;
+  }
+
+  static double lerpDegrees(double start, double target, double duration, double timeSinceStart) {
+    double result;
+    final range = target - start;
+    if (range < -180) {
+      target += 360;
+      result = lerp(start, target, duration, timeSinceStart);
+      if (result >= 360) {
+        result -= 360;
+      }
+    } else if (range > 180) {
+      target -= 360;
+      result = lerp(start, target, duration, timeSinceStart);
+      if (result < 0) {
+        result += 360;
+      }
+    } else {
+      result = lerp(start, target, duration, timeSinceStart);
+    }
+    return result;
   }
 }
--- lib/src/core/math.dart
+++ lib/src/core/math.dart
@@ -2,7 +2,7 @@ import 'dart:math';
 import 'dart:ui';
 
 import 'package:vector_math/vector_math_64.dart';
-
+export 'dart:math';
 export 'package:vector_math/vector_math_64.dart' show Vector2;
 
 const double twoPi = pi * 2;
