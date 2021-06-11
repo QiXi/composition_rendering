cadf65ff1558abcbc916e71e41c4168d157bb858
Stanislav <qixi@mail.ru>
2021-06-11 17:45:15 +0300

    added minimal example

--- example/engine_example.dart
+++ example/engine_example.dart
@@ -1,5 +1,19 @@
-
+import 'package:composition_rendering/core.dart';
+import 'package:composition_rendering/scene.dart';
 
 void main() {
+  // Creating an action scene in your game
+  final gameScene = GameScene();
+}
 
+class GameScene extends Scene {
+  @override
+  void init() {
+    // get drawing area from picture
+    var texture = systems.textureSystem.getTextureRegion('bg.jpg');
+    // create a scene object as a sprite
+    var background = systems.factorySystem.spawnSprite(texture!, Priority.background, scale: 0.5);
+    // add an object to the scene to display
+    add(background);
+  }
 }
